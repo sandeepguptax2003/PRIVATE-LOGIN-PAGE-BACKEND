00b96b1003584cd591cf4ca35fda4fc28cecaea6
sandeep <sandeepguptax2003@gmail.com>
2024-10-10 17:07:47 +0530

    Added Private Domain

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth');
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL, // replace with your client's URL
-  credentials: true
-}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
